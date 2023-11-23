3f1ec1d19414d3d7c3f0c065f2ca641caa80e502
Steve Rodrigue <stev.rodr@gmail.com>
2023-11-22 22:56:58 -0500

    Added the credentials structs

--- domain/credentials/sdk.go
+++ domain/credentials/sdk.go
@@ -1,5 +1,18 @@
 package credentials
 
+// NewBuilder creates a new builder instance
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// Builder represents the credentials builder
+type Builder interface {
+	Create() Builder
+	WithUsername(username string) Builder
+	WithPassword(password []byte) Builder
+	Now() (Credentials, error)
+}
+
 // Credentials represents credentials
 type Credentials interface {
 	Username() string
