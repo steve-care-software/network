777ea19bf1b919b7ab50880d4f64d4e211ea3939
Steve Rodrigue <stev.rodr@gmail.com>
2023-12-04 03:49:50 -0500

    Added the remaining query resources

--- domain/databases/resources/queries/sdk.go
+++ domain/databases/resources/queries/sdk.go
@@ -2,6 +2,7 @@ package queries
 
 import (
 	"steve.care/network/domain/databases/criterias"
+	"steve.care/network/domain/databases/criterias/conditions"
 	"steve.care/network/domain/hash"
 )
 
@@ -10,4 +11,18 @@ type Query interface {
 	Hash() hash.Hash
 	IsCriteria() bool
 	Criteria() criterias.Criteria
+	IsCondition() bool
+	Condition() conditions.Condition
+	IsPointer() bool
+	Pointer() conditions.Pointer
+	IsElement() bool
+	Element() conditions.Element
+	IsResource() bool
+	Resource() conditions.Resource
+	IsOperator() bool
+	Operator() conditions.Operator
+	IsRelationalOperator() bool
+	RelationalOperator() conditions.RelationalOperator
+	IsIntegerOperator() bool
+	IntegerOperator() conditions.IntegerOperator
 }
