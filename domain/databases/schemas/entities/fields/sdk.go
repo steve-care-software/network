adc159cf558f77113a6204142ec3c3ddc98a59ce
Steve Rodrigue <stev.rodr@gmail.com>
2023-12-04 06:18:21 -0500

    Added the field structs

--- domain/databases/schemas/entities/fields/sdk.go
+++ domain/databases/schemas/entities/fields/sdk.go
@@ -2,6 +2,16 @@ package fields
 
 import "steve.care/network/domain/databases/schemas/entities/fields/kinds"
 
+// NewBuilder creates a new builder instance
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// NewFieldBuilder creates a new field instance
+func NewFieldBuilder() FieldBuilder {
+	return createFieldBuilder()
+}
+
 // Builder represents the fields builder
 type Builder interface {
 	Create() Builder
