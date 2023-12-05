666bfe5234b89e4fb840e0f4d836e64cb5697907
Steve Rodrigue <stev.rodr@gmail.com>
2023-12-05 03:32:06 -0500

    Added the resource layer builder interface

--- domain/databases/resources/layers/sdk.go
+++ domain/databases/resources/layers/sdk.go
@@ -5,6 +5,28 @@ import (
 	"steve.care/network/domain/receipts/commands/layers"
 )
 
+// Builder represents a layer builder
+type Builder interface {
+	Create() Builder
+	WithLayer(layer layers.Layer) Builder
+	WithOutput(output layers.Output) Builder
+	WithKind(kind layers.Kind) Builder
+	WithInstruction(ins layers.Instruction) Builder
+	WithLayerInstructions(layerIns layers.LayerInstruction) Builder
+	WithCondition(condition layers.Condition) Builder
+	WithAssignment(assignment layers.Assignment) Builder
+	WithAssignable(assignable layers.Assignable) Builder
+	WithBytes(bytes layers.Bytes) Builder
+	WithIdentity(identity layers.Identity) Builder
+	WithEncryptor(encryptor layers.Encryptor) Builder
+	WithSigner(signer layers.Signer) Builder
+	WithSignatureVerify(sigVerify layers.SignatureVerify) Builder
+	WithVoteVerify(voteVerify layers.VoteVerify) Builder
+	WithVote(vote layers.Vote) Builder
+	WithBytesReference(bytesReference layers.BytesReference) Builder
+	Now() (Layer, error)
+}
+
 // Layer represents a layer resource
 type Layer interface {
 	Hash() hash.Hash
