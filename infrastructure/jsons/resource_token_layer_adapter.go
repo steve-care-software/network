07d72f56939327109334d5ba037aa82b5b51a16e
Steve Rodrigue <stev.rodr@gmail.com>
2023-12-14 08:35:29 -0500

    Added the vote to the adapter

--- infrastructure/jsons/resource_token_layer_adapter.go
+++ infrastructure/jsons/resource_token_layer_adapter.go
@@ -7,6 +7,7 @@ import (
 )
 
 type resourceTokenLayerAdapter struct {
+	voteBuilder           layers.VoteBuilder
 	bytesReferenceBuilder layers.BytesReferenceBuilder
 }
 
@@ -18,6 +19,30 @@ func (app *resourceTokenLayerAdapter) ToStruct(ins resources_layers.Layer) struc
 // ToInstance converts bytes to resource layer instance
 func (app *resourceTokenLayerAdapter) ToInstance(ins structs_layers.Layer) (resources_layers.Layer, error) {
 	return nil, nil
+}
+
+func (app *resourceTokenLayerAdapter) voteToStruct(
+	ins layers.Vote,
+) structs_layers.Vote {
+	message := app.bytesReferenceToStruct(ins.Message())
+	return structs_layers.Vote{
+		Ring:    ins.Ring(),
+		Message: message,
+	}
+}
+
+func (app *resourceTokenLayerAdapter) structToVote(
+	ins structs_layers.Vote,
+) (layers.Vote, error) {
+	message, err := app.structToBytesReference(ins.Message)
+	if err != nil {
+		return nil, err
+	}
+
+	return app.voteBuilder.Create().
+		WithMessage(message).
+		WithRing(ins.Ring).
+		Now()
 }
 
 func (app *resourceTokenLayerAdapter) bytesReferenceToStruct(
