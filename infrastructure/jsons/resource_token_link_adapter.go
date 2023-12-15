9d16e7ad6cdc88f7d3f08d5b5fe28a02995c41ea
Steve Rodrigue <stev.rodr@gmail.com>
2023-12-15 06:26:21 -0500

    Added the link origin resource json adapter

--- infrastructure/jsons/resource_token_link_adapter.go
+++ infrastructure/jsons/resource_token_link_adapter.go
@@ -1,12 +1,15 @@
 package jsons
 
 import (
+	"steve.care/network/domain/hash"
 	"steve.care/network/domain/receipts/commands/links"
 	structs_links "steve.care/network/infrastructure/jsons/resources/tokens/links"
 )
 
 type resourceTokenLinkAdapter struct {
-	operatorBuilder links.OperatorBuilder
+	hashAdapter           hash.Adapter
+	originResourceBuilder links.OriginResourceBuilder
+	operatorBuilder       links.OperatorBuilder
 }
 
 // LinkToStruct converts a link to struct
@@ -21,6 +24,33 @@ func (app *resourceTokenLinkAdapter) StructToLink(
 	ins structs_links.Link,
 ) (links.Link, error) {
 	return nil, nil
+}
+
+func (app *resourceTokenLinkAdapter) originResourceToStruct(
+	ins links.OriginResource,
+) structs_links.OriginResource {
+	return structs_links.OriginResource{
+		Layer:       ins.Hash().String(),
+		IsMandatory: ins.IsMandatory(),
+	}
+}
+
+func (app *resourceTokenLinkAdapter) structToOriginResource(
+	ins structs_links.OriginResource,
+) (links.OriginResource, error) {
+	pLayerHash, err := app.hashAdapter.FromString(ins.Layer)
+	if err != nil {
+		return nil, err
+	}
+
+	builder := app.originResourceBuilder.Create().
+		WithLayer(*pLayerHash)
+
+	if ins.IsMandatory {
+		builder.IsMandatory()
+	}
+
+	return builder.Now()
 }
 
 func (app *resourceTokenLinkAdapter) operatorToStruct(
@@ -47,15 +77,15 @@ func (app *resourceTokenLinkAdapter) structToOperator(
 ) (links.Operator, error) {
 	builder := app.operatorBuilder.Create()
 	if ins.And {
-
+		builder.IsAnd()
 	}
 
 	if ins.Or {
-
+		builder.IsOr()
 	}
 
 	if ins.Xor {
-
+		builder.IsXor()
 	}
 
 	return builder.Now()
